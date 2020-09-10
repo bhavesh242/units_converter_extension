492fecd33c721726006df873a523badf100bee33
Bhavesh Agrawal <bhavesh.agrawal242@gmail.com>
2020-09-10 19:40:15 -0400

    Remove Redundant Files

--- conversion_scripts.js
+++
@@ -1,9 +0,0 @@
-function get_conversions(text){
-
-    //Regular expression to match the numerical part and optional space
-    const numSpaceRegExp = new RegExp(/((^[\-−]?(?:\d+|\d{1,3}(?:,\d{3})+)(?:(\.|,)\d+)?))\s*/);
-    let result="";
-    
-
-
-}
